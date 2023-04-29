9a56746fde8acf67e2a6ef5cf5807aa7a7c0ba5c
vanshbajaj13 <vanshbajaj705@gmail.com>
2023-04-29 15:35:43 +0530

    added ranker section

---
+++ frontend/src/Components/TopRanker/Ranker.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Ranker = () => {
+  return (
+    <div className="ranker">
+      <div className="infoDiv">
+        <div className="imgDiv">
+          <img src="./Images/student.jpg" alt="ranker1"></img>
+        </div>
+        <div className="scoreDiv">
+        <h2>Vansh Bajaj</h2>
+        <p>Phy - 95</p>
+        <p>Chem - 94</p>
+        <p>Math - 95</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ranker;
--- frontend/src/Components/TopRanker/TopRankerPage.jsx
+++ frontend/src/Components/TopRanker/TopRankerPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Ranker from "./Ranker";
 
 const TopRankerPage = () => {
   return (
@@ -7,7 +8,11 @@ const TopRankerPage = () => {
         <div className="rankerHeading">
           <h1>Our Top Rankers</h1>
         </div>
-        <div className="rankersDiv"></div>
+        <div className="rankersDiv">
+            <Ranker></Ranker>
+            <Ranker></Ranker>
+            <Ranker></Ranker>
+        </div>
       </div>
     </section>
   );
